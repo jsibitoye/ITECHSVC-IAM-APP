eeef75a5e468bfc2faf0dc44dbc11fc4f6a1f61e
J.s Ibitoye <jsibitoye@outlook.com>
2024-03-08 20:54:42 -0600

    Add supporting project files [12]

---
+++ src/test/java/com/iTechSVC/iTechSVC_iam_app/ITechSvcIamAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iTechSVC.iTechSVC_iam_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ITechSvcIamAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
